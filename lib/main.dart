1ea249c68d6e7fe759c47facc642f39460d95d09
Moreira <valdosm.moreira@gmail.com>
2024-03-26 12:19:19 -0300

    formatando texto

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,30 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Frses do dia",
-    home: Column(children: <Widget> [
-      Text("Usuario"),
-      Text("Senha")
-    ],),
-    //home: Container(color: Colors.white,),
+    home: Container(
+      color: Colors.yellow,
+      child:Column(
+        children: <Widget>[
+          Text(""
+              "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,",
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 40,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.normal,
+              letterSpacing: 1,//espaco entre as letras
+              wordSpacing: 20,//espaçamento entre palavras
+              decoration: TextDecoration.lineThrough,//marca ou nãp o texto
+              decorationColor: Colors.black//muda a cor da inha que marca otexto
 
-  ));
+
+            )
+
+          ),
+
+
+        ],
+      ) ,
+    )
+    ));
 }
