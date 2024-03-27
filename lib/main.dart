6f748f29ef4cf944e61b433e4096ba08246396bd
Moreira <valdosm.moreira@gmail.com>
2024-03-27 19:40:29 -0300

    espaçamento

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,23 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
+    //debugShowCheckedModeBanner: false,
     title: "Frses do dia",
     home: Container(
-      color: Colors.yellow,
+      padding: EdgeInsets.fromLTRB(100, 30, 0, 0),
+     // margin: EdgeInsets.all(20),
+      margin: EdgeInsets.only(top:100, left: 20),//somente em alguns locals
+      decoration: BoxDecoration(
+        border: Border.all(width:5,  color: Colors.red)
+      ),
       child:Column(
         children: <Widget>[
           Text(""
               "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,",
+            textAlign: TextAlign.justify,
             style: TextStyle(
               color: Colors.cyan,
-              fontSize: 40,
+              fontSize: 30,
               fontStyle: FontStyle.italic,
               fontWeight: FontWeight.normal,
               letterSpacing: 1,//espaco entre as letras
@@ -30,7 +37,7 @@ void main(){
                fontSize: 40,
                fontStyle: FontStyle.italic,
                fontWeight: FontWeight.normal,
-               decorationColor: Colors.black,//muda a cor da inha que marca otexto
+               decorationColor: Colors.yellow,//muda a cor da linha que marca otexto
                backgroundColor: Colors.red,
 
 
